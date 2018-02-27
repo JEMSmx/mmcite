689785ecb1b14a607c3a60462e07e9adb13fba48
jesaa <jesaa@me.com>
2018-02-27 02:39:21 -0600

    Add contact

---
+++ public/site/templates/contact.php
@@ -0,0 +1,204 @@
+<?php
+
+include('./_head.php'); 
+
+include('./_nav.php');  ?>
+
+  <!--  Hero -->
+  <div class="hero-content-products">
+    <div class="contacto-slick">
+      <div class="hero-contact-item-1">
+        <h2 class="item-title">CONTACT</h2>
+      </div>
+      <div class="hero-contact-item-2">
+        <h2 class="item-title">CONTACT</h2>
+      </div>
+      <div class="hero-contact-item-3">
+        <h2 class="item-title">CONTACT</h2>
+      </div>
+      <div class="hero-contact-item-4">
+        <h2 class="item-title">CONTACT</h2>
+      </div>
+    </div>
+  </div>
+  <!--  Main content -->
+  <main class="j-workspace ">
+    <div class="j-wrap">
+      <!-- section welcome -->
+      <div class="menu-a">
+        <h3><a href="/" class="inactive">HOME</a></h3>
+        <p>&nbsp;&nbsp;>>&nbsp;&nbsp;</p>
+        <h3><a href="/contact" class="active">CONTACT</a></h3>
+      </div>
+      <!-- contact section -->
+      <div class="contact">
+        <div class="accordion">
+          <div class="half-accordion">
+            <div class="tab blue">
+              <input id="tab-1" type="radio" name="tabs2" checked>
+              <label for="tab-1">Contact Address</label>
+              <div class="tab-content">
+                <h4>mmcité1 a.s (<a href="#">Map</a>)</h4>
+                <div class="info-content">
+                  <div class="info-icon">
+                    <img src="https://dummyimage.com/12x12/000/fff" alt="">
+                  </div>
+                  <div class="info-text">
+                    <p>Bilovice 519</p>
+                    <p>687 12 Bilovice</p>
+                    <p>Czech Republic</p>
+                  </div>
+                </div>
+                <div class="info-content">
+                  <div class="info-icon">
+                    <img src="https://dummyimage.com/12x12/000/fff" alt="">
+                  </div>
+                  <div class="info-text">
+                    <p>IC 276 70 864</p>
+                    <p>DIC CZ276 70 864</p>
+                    <p>registered in the Commercial Register kept at the</p>
+                    <p>Regional Court in Brno. under file number B4523</p>
+                  </div>
+                </div>
+                <h4 class="h4-second">mmcité + as (<a href="#">Map</a>)</h4>
+                <div class="info-content">
+                  <div class="info-icon">
+                    <img src="https://dummyimage.com/12x12/000/fff" alt="">
+                  </div>
+                  <div class="info-text">
+                    <p>with its registered office at Bilovice 519</p>
+                    <p>687 12 Bilovice</p>
+                    <p>Czech Republic</p>
+                  </div>
+                </div>
+                <div class="info-content">
+                  <div class="info-icon">
+                    <img src="https://dummyimage.com/12x12/000/fff" alt="">
+                  </div>
+                  <div class="info-text">
+                    <p>+420 572 434 292</p>
+                  </div>
+                </div>
+                <div class="info-content">
+                  <div class="info-icon">
+                    <img src="https://dummyimage.com/12x12/000/fff" alt="">
+                  </div>
+                  <div class="info-text">
+                    <p>info@mmciteplus.com</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="tab blue">
+              <input id="tab-2" type="radio" name="tabs2">
+              <label for="tab-2" class="top-label">Contact Persons -</label>
+              <label for="tab-2" class="sublabel">Urban furniture - domestic</label>
+              <div class="tab-content">
+                <h4>mmcité1 a.s (<a href="#">Map</a>)</h4>
+                <div class="info-content">
+                  <div class="info-icon">
+                    <img src="https://dummyimage.com/12x12/000/fff" alt="">
+                  </div>
+                  <div class="info-text">
+                    <p>Bilovice 519</p>
+                    <p>687 12 Bilovice</p>
+                    <p>Czech Republic</p>
+                  </div>
+                </div>
+                <div class="info-content">
+                  <div class="info-icon">
+                    <img src="https://dummyimage.com/12x12/000/fff" alt="">
+                  </div>
+                  <div class="info-text">
+                    <p>IC 276 70 864</p>
+                    <p>DIC CZ276 70 864</p>
+                    <p>registered in the Commercial Register kept at the</p>
+                    <p>Regional Court in Brno. under file number B4523</p>
+                  </div>
+                </div>
+                <h4 class="h4-second">mmcité + as (<a href="#">Map</a>)</h4>
+                <div class="info-content">
+                  <div class="info-icon">
+                    <img src="https://dummyimage.com/12x12/000/fff" alt="">
+                  </div>
+                  <div class="info-text">
+                    <p>with its registered office at Bilovice 519</p>
+                    <p>687 12 Bilovice</p>
+                    <p>Czech Republic</p>
+                  </div>
+                </div>
+                <div class="info-content">
+                  <div class="info-icon">
+                    <img src="https://dummyimage.com/12x12/000/fff" alt="">
+                  </div>
+                  <div class="info-text">
+                    <p>+420 572 434 292</p>
+                  </div>
+                </div>
+                <div class="info-content">
+                  <div class="info-icon">
+                    <img src="https://dummyimage.com/12x12/000/fff" alt="">
+                  </div>
+                  <div class="info-text">
+                    <p>info@mmciteplus.com</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="tab blue">
+              <input id="tab-3" type="radio" name="tabs2">
+              <label for="tab-3" class="top-label">Contact Person -</label>
+              <label for="tab-3" class="sublabel">Urban furniture - export</label>
+              <div class="tab-content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
+              </div>
+            </div>
+            <div class="tab blue">
+              <input id="tab-4" type="radio" name="tabs2">
+              <label for="tab-4" class="top-label">Contact Persons mmcite +</label>
+              <label for="tab-4" class="sublabel">- Infrastructure projects</label>
+              <div class="tab-content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
+              </div>
+            </div>
+            <div class="tab blue">
+              <input id="tab-5" type="radio" name="tabs2">
+              <label for="tab-5">Representation</label>
+              <div class="tab-content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="contact-content">
+          <h2>WE WOULD LOVE TO HEAR FROM YOU:</h2>
+          <form class="contact-form" id="contact-form" action="#" method="POST">
+            <div class="input-content">
+              <input type="text" placeholder="Your Name*" name="name">
+            </div>
+            <div class="input-content">
+              <input type="email" placeholder="Email*" name="email">
+            </div>
+            <input type="number" placeholder="Phone Number" name="number">
+            <div class="input-content">
+              <input type="text" placeholder="Subject*" name="subject">
+            </div>
+            <select name="select1" id="select1">
+              <option value="lorem" disabled selected hidden>Location</option>
+              <option value="lorem 2">Lorem 1</option>
+              <option value="lorem 2">Lorem 2</option>
+            </select>
+            <div class="contact-textarea">
+              <label for="contact-area">Message:</label>
+              <textarea name="contact-area" id="contact-area" cols="30" rows="5"></textarea>
+            </div>
+            <button type="submit">SUBMIT</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    <!-- section banner -->
+    <?php include('./_subs.php'); ?>
+  </main>
+  
+<?php include('./_foot.php'); ?>
